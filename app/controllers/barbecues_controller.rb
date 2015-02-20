680d4838247f8f339c814e3490c6ab14726ade6a
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-02-20 06:59:02 -0500

    Add barbecue save logic

--- app/controllers/barbecues_controller.rb
+++ app/controllers/barbecues_controller.rb
@@ -6,4 +6,15 @@ class BarbecuesController < ApplicationController
   def new
     @bbq = Barbecue.new
   end
+
+  def create
+    bbq_params = params.require(:barbecue).permit(:title, :venue, :date)
+    @bbq = Barbecue.new(bbq_params)
+
+    unless @bbq.save
+      render(:new)
+    else
+      redirect_to(barbecues_path)
+    end
+  end
 end
