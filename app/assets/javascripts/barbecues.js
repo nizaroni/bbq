ea06f063a15f705edab387c4063df9abe7d8d74f
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-02-20 07:25:42 -0500

    Add BBQ join JavaScript

--- app/assets/javascripts/barbecues.js
+++ app/assets/javascripts/barbecues.js
@@ -1,2 +1,28 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+(function main () {
+  var isJoining = false
+
+  $('[data-hook~=join-bbq]').on('click', function (event) {
+    if (isJoining) {
+      return
+    }
+
+    isJoining = true
+    var $button = $(event.target)
+    var bbqId = $button.closest('[data-bbq]').data('bbq')
+
+    var request = $.post('/api/barbecues/' + bbqId + '/join')
+
+    request.fail(function () {
+      alert('Couldn’t join the barbecue. Try again later.')
+      isJoining = false
+    })
+
+    request.done(function () {
+      $button.fadeOut()
+      isJoining = false
+    })
+  })
+})()
